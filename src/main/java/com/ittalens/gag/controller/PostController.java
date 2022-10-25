1dfb864365d7144e508c88cb8b4881f031af83e8
MitkoTotev <dimitarttotev@gmail.com>
2022-10-25 17:35:47 +0300

    added getting paged posts by tag, date, category

--- src/main/java/com/ittalens/gag/controller/PostController.java
+++ src/main/java/com/ittalens/gag/controller/PostController.java
@@ -8,6 +8,7 @@ import com.ittalens.gag.services.PostService;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +29,7 @@ public class PostController {
     @PostMapping("/upload")
     private ResponseEntity<?> uploadFile(@ModelAttribute PostCreateReqDTO dto, HttpSession session) {
         postService.createPost(dto, Long.parseLong(session.getAttribute("USER_ID").toString()));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("Post is uploaded!");
     }
 
     @GetMapping("/all")
@@ -36,15 +37,14 @@ public class PostController {
         return postService.getAllPostsDto();
     }
 
-    @GetMapping("/date")
-    private List<PostRespDTO> getAllPostsByDateAndTime() {
-        return postService.getAllByCreationDate();
+    @GetMapping("/date/{offset}/{pageSize}")
+    private Page<PostRespDTO> getAllPostsByDateAndTime(@PathVariable int offset, @PathVariable int pageSize) {
+        return postService.getAllByCreationDate(offset, pageSize);
     }
 
-    @GetMapping("/{word}")
-    private ResponseEntity<?> getAllPostsByWord(@PathVariable String word) {
-        List<PostRespDTO> posts = postService.findPostsByWord(word);
-        return ResponseEntity.ok(posts);
+    @GetMapping("/{word}/")
+    private ResponseEntity<?> getAllPostsByWord(@PathVariable String word, @PathVariable int offset, @PathVariable int pageSize) {
+        return ResponseEntity.ok(postService.findPostsByWord(word, offset, pageSize));
     }
 
     @DeleteMapping("/delete/{id}")
@@ -72,4 +72,13 @@ public class PostController {
         return ResponseEntity.ok(postRespDTO);
     }
 
+    @GetMapping("/category/{categoryId}/{offset}/{pageSize}")
+    private Page<PostRespDTO> getAllPostsCategory(@PathVariable Long categoryId, @PathVariable int offset, @PathVariable int pageSize) {
+        return postService.getAllPostsCategory(categoryId, offset, pageSize);
+    }
+
+    @GetMapping("/tag/{type}/{offset}/{pageSize}")
+    private Page<PostRespDTO> getAllPostsWithTags(@PathVariable String type, @PathVariable int offset, @PathVariable int pageSize) {
+        return postService.allPostsWithTag(type, offset, pageSize);
+    }
 }
